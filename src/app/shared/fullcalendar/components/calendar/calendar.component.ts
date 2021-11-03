bcebef34ca254a0e604372226ecb01321a146904
emilioCode <emilio_mem@hotmail.com>
2021-11-03 00:42:55 -0400

    init

---
+++ src/app/shared/fullcalendar/components/calendar/calendar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
